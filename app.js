0e87f6013bcbf916158394a8f1e4b14ed3dcc9ee
grovatti <guillermo.rovatti@gmail.com>
2022-01-26 20:12:01 -0300

    feat: tarea 11

--- app.js
+++ app.js
@@ -3,14 +3,12 @@ const { argv } = require('./config/yargs');
 const { grabarArchivo, eliminarRegistro } = require('./archivos/archivo');
 
 const persona = {
-    nombre: argv.nombre,
-    apellido: argv.apellido,
-    edad: argv.edad
+  nombre: argv.nombre,
+  apellido: argv.apellido,
+  edad: argv.edad,
 };
 
-let nombreArchivo = persona.edad >= 45 ? 'mayores45.txt' : 'menores45.txt';
+const nombreArchivo = persona.edad >= 45 ? 'mayores45.txt' : 'menores45.txt';
 
-if (!argv.delete)
-    grabarArchivo(nombreArchivo, persona);
-else
-    eliminarRegistro(nombreArchivo, persona);
+// eslint-disable-next-line max-len
+if (!argv.delete) { grabarArchivo(nombreArchivo, persona); } else { eliminarRegistro(nombreArchivo, persona); }
--- archivos/archivo.js
+++ archivos/archivo.js
@@ -1,30 +1,29 @@
 const fs = require('fs');
 
 const grabarArchivo = (archivo, persona) => {
-    let datos = `Nombre: ${persona.nombre} Apellido: ${persona.apellido} Edad: ${persona.edad}\n`;
-    fs.appendFile(archivo, datos, 'utf8', (err) => {
-        if (err) console.log(`Error al grabar el archivo. ${err.message}`.red)
-        console.log(`Se agrego un nuevo registro al archivo ${archivo}`.green);
-    })
-}
+  const datos = `Nombre: ${persona.nombre} Apellido: ${persona.apellido} Edad: ${persona.edad}\n`;
+  fs.appendFile(archivo, datos, 'utf8', (err) => {
+    if (err) console.log(`Error al grabar el archivo. ${err.message}`.red);
+    console.log(`Se agrego un nuevo registro al archivo ${archivo}`.green);
+  });
+};
 
 const eliminarRegistro = (archivo, persona) => {
-    let reg = `Nombre: ${persona.nombre} Apellido: ${persona.apellido} Edad: ${persona.edad}`;
-    fs.readFile(archivo, (err, data) => {
-        let registros = String(data).split('\n');
-        let nuevosRegistros = registros.filter(e => !e.includes(reg));
-        if (nuevosRegistros.length != registros.length) {
-            const datos = nuevosRegistros.join('\n');
-            fs.writeFile(archivo, datos, 'utf8', (err) => {
-                if (err) console.log(`Error al grabar el archivo. ${err.message}`.red)
-                console.log(`Se actualizó el archivo ${archivo}!!!`.yellow);
-            });
-        } else
-            console.log(`No se realizo ninguna accion sobre el archivo ${archivo}`.grey);
-    });
-}
+  const reg = `Nombre: ${persona.nombre} Apellido: ${persona.apellido} Edad: ${persona.edad}`;
+  fs.readFile(archivo, (err, data) => {
+    const registros = String(data).split('\n');
+    const nuevosRegistros = registros.filter((e) => !e.includes(reg));
+    if (nuevosRegistros.length != registros.length) {
+      const datos = nuevosRegistros.join('\n');
+      fs.writeFile(archivo, datos, 'utf8', (err) => {
+        if (err) console.log(`Error al grabar el archivo. ${err.message}`.red);
+        console.log(`Se actualizó el archivo ${archivo}!!!`.yellow);
+      });
+    } else { console.log(`No se realizo ninguna accion sobre el archivo ${archivo}`.grey); }
+  });
+};
 
 module.exports = {
-    grabarArchivo,
-    eliminarRegistro
-}
+  grabarArchivo,
+  eliminarRegistro,
+};
